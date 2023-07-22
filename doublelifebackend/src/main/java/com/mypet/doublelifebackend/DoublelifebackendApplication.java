7ac3c394f637df2755fc7642cfd5369ea10355cb
sh <rigollet153@gmail.com>
2023-07-22 23:00:50 +0900

    Make Project

---
+++ doublelifebackend/src/main/java/com/mypet/doublelifebackend/DoublelifebackendApplication.java
@@ -0,0 +1,13 @@
+package com.mypet.doublelifebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublelifebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublelifebackendApplication.class, args);
+	}
+
+}
---
+++ doublelifebackend/src/test/java/com/mypet/doublelifebackend/DoublelifebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypet.doublelifebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoublelifebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
