afffe88a48b98b5600be9dd0cf6b18e2c8d4bcec
sh <rigollet153@gmail.com>
2023-07-28 00:19:50 +0900

    Set proxy server

---
+++ doublelifebackend/src/main/java/com/mypet/doublelifebackend/TestController.java
@@ -0,0 +1,17 @@
+package com.mypet.doublelifebackend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/test/hello")
+	@ResponseBody
+	public String test(Model model) {
+		return "Test";
+	}
+	
+}
--- doublelifefrontend/src/App.tsx
+++ doublelifefrontend/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+    useEffect(() => {
+      fetch('/test/hello')
+          .then(response => response.text())
+          .then(message => {
+              setMessage(message);
+          });
+    },[]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{message}</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
