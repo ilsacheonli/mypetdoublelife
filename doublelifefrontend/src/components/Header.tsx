45c2c483f40b5c858f12abbb4a238217b806e635
sh <rigollet153@gmail.com>
2023-08-18 14:35:31 +0900

    헤더 수정

--- doublelifefrontend/src/components/Header.tsx
+++ doublelifefrontend/src/components/Header.tsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-
-const NavContainer = styled.nav`
-  width: 100%;
-  backdrop-filter: saturate(180%) blur(20px);
-  color: ${props => props.theme.lightGray};
-  height: 44px;
-`
-
-const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  max-width: 980px;
-  margin: 0 auto;
-  padding: 0 20px;
-`
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { LinkItems } from "./LinkItems";
+import { NavContainer, Content, Logo, Links, LinkSign } from "GlobalStyle";
 
 function Header() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <NavContainer>
+        <Content>
+          <a href="/">
+            <Logo />
+          </a>
+          <Links>
+            {LinkItems.map((item, index) => (
+              <li key={index}>
+                <a className="link" href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </Links>
+          <LinkSign>
+            <li>
+              <a className="link" href="/login">로그인</a>
+              <span className="vl" />
+              <a className="link" href="/signup">회원가입</a>
+            </li>
+          </LinkSign>
+        </Content>
+      </NavContainer>
+    </>
+  );
 }
 
 export default Header;
--- doublelifefrontend/src/pages/aboutus/AboutUs.tsx
+++ doublelifefrontend/src/pages/aboutus/AboutUs.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function AboutUs() {
     return (
         <div>
-            
+            <h1>test landing page</h1>
         </div>
     );
 }
