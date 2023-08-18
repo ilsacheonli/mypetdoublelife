11d07c05611fd09ec68442a878538f4c2c618a1a
sh <rigollet153@gmail.com>
2023-08-18 14:35:31 +0900

    헤더 수정

--- doublelifefrontend/src/components/App.tsx
+++ doublelifefrontend/src/components/App.tsx
@@ -3,10 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routers } from '../routes/Routers';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import { GlobalStyle } from 'GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Header />
       <Routers />
       <Footer />
