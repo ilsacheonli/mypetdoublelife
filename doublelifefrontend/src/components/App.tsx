aa6f43a3fb47a38222bac1dabbb0a23eccf256a2
sh <rigollet153@gmail.com>
2023-08-25 15:33:03 +0900

    App.tsx: GlobalStyle 삭제

--- doublelifefrontend/src/components/App.tsx
+++ doublelifefrontend/src/components/App.tsx
@@ -3,12 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routers } from '../routes/Routers';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-import { GlobalStyle } from 'GlobalStyle';
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
       <Header />
       <Routers />
       <Footer />
