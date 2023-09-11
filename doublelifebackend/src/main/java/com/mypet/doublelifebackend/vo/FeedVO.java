0126a799ca973366063748630ddfc4cb205e53a7
chldpwl29 <choiyeji29@gmail.com>
2023-09-11 15:44:25 +0900

    2023.09.11 FeedVO 오류 수정 버전 백업

--- doublelifebackend/src/main/java/com/mypet/doublelifebackend/vo/FeedVO.java
+++ doublelifebackend/src/main/java/com/mypet/doublelifebackend/vo/FeedVO.java
@@ -16,12 +16,15 @@ public class FeedVO {
     private int likeNum; //좋아요 수
     private String contentTitle; // 피드 제목
     private LocalDateTime regTime; // 피드 등록 시간
-    private String contentText; // 피드 내용 //그래서 이거 한줄 더 추가해준.. 그리고 나서 모든 타입이 인식이 안되는...
+    private String contentText; // 피드 내용
 
 
     //private String contentImage; // 피드 콘텐츠 이미지
     //private String user_image; // 사용자 프로필 사진
 
+    public FeedVO(){
+
+    }
 
     public FeedVO(String user_id, String user_name, int likeNum, String contentTitle, LocalDateTime regTime, String contentText) {
         this.user_id = user_id;
