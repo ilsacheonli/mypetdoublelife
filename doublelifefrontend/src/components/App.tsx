6d65367d4b59c759efae1b6d5f9fb489d87df24f
sh <rigollet153@gmail.com>
2023-09-15 03:42:46 +0900

    footer 조건 설정

--- doublelifefrontend/src/components/App.tsx
+++ doublelifefrontend/src/components/App.tsx
@@ -4,18 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routers } from '../routes/Routers';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import { useLocation } from 'react-router-dom';
 // import ErrorBoundary from 'pages/errorpage/ErrorBoundary';
 
 function App() {
+  // const locationNow = useLocation();
   return (
     
     <div className="App">
-      
-  
-      {/* <ErrorBoundary>     */}
+
+      {window.location.pathname === "/petmap/salon" || window.location.pathname === "/petmap/hospital"
+      ? <>
+      <Header />
+      <Routers />
+      </>
+      :
+      <>
       <Header />
       <Routers />
       <Footer />
+      </>
+      }
+      
+  
+      {/* <ErrorBoundary>     */}
+      {/* <Header />
+      <Routers />
+      <Footer /> */}
      
       
      
